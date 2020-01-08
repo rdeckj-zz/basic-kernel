a2d7fc5f28c31b4304e07f45d4df650d8506ae31
Josh <Rdeckj@gmail.com>
2020-01-07 21:07:30 -0600

    Rename kernel.asm to boot.asm and slightly modify code

---
+++ boot.asm
@@ -0,0 +1,24 @@
+MAGIC      equ 0x1BADB002
+FLAGS      equ 0x00
+CHECKSUM   equ -(MAGIC + FLAGS)
+
+section    .multiboot
+	   align 4                ; align at 32-bit boundary
+	   dd MAGIC
+	   dd FLAGS
+	   dd CHECKSUM
+
+section    .text
+global     start
+extern     kmain
+
+start:	   mov esp, stack_top     ; set stack pointer
+	   call kmain		  ; call c function
+	   cli                    ; disable interrupts
+hang:	   hlt			  ; halt the CPU
+	   jmp hang		
+
+section    .bss
+stack_bot:		                             
+resb 8192	                  ; reserve 8KB for stack
+stack_top:
