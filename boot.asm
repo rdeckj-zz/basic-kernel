5f53849cd4405d1fef7a4aed6bced988424af966
Josh Radecki <rdeckj@gmail.com>
2020-01-26 22:17:25 -0600

    Add .gitignore and fix spacing

--- boot.asm
+++ boot.asm
@@ -3,22 +3,22 @@ FLAGS      equ 0x00
 CHECKSUM   equ -(MAGIC + FLAGS)
 
 section    .multiboot
-	   align 4                ; align at 32-bit boundary
-	   dd MAGIC
-	   dd FLAGS
-	   dd CHECKSUM
+           align 4                ; align at 32-bit boundary
+	       dd MAGIC
+	       dd FLAGS
+	       dd CHECKSUM
 
 section    .text
 global     start
 extern     kmain
 
-start:	   mov esp, stack_top     ; set stack pointer
-	   call kmain		  ; call c function
-	   cli                    ; disable interrupts
-hang:	   hlt			  ; halt the CPU
-	   jmp hang		
+start:     mov esp, stack_top     ; set stack pointer
+           call kmain             ; call c function
+           cli                    ; disable interrupts
+hang:      hlt			          ; halt the CPU
+           jmp hang
 
 section    .bss
 stack_bot:		                             
-resb 8192	                  ; reserve 8KB for stack
+           resb 8192	          ; reserve 8KB for stack
 stack_top:
